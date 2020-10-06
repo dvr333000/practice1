1f40cf279d6a979bce3f8c649bfb10ba20af6daa
dvr333000 <dvr333000@gmail.com>
2020-10-06 22:14:33 +0530

    new files added

---
+++ Demo2.py
@@ -0,0 +1,17 @@
+my_name = 'Raghu'
+my_height = 5.5
+my_weight = 70
+my_age = 35
+my_phone = 'Samsung S9'
+
+print("Myself " + my_name)
+print('and my height', my_height, "ft and weight in Kgs", my_weight)
+print("My age is %d years and I use %s phone" % (my_age, my_phone))
+
+a = 10
+b = 50.55
+c = 100+3j
+
+print(a, type(a))
+print(b, type(b))
+print(c, type(c))
